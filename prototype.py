e1977e925f558e3bd67fd14aa953a662ca1b95ca
Terminatoror <122676845+Terminatoror@users.noreply.github.com>
2023-07-22 22:43:01 +0200

    fixed bug + added new version

--- prototype.py
+++ prototype.py
@@ -1,13 +1,13 @@
 import re
 import time
 import logging
+import subprocess
 from pwnagotchi import plugins, config
 from pwnagotchi.plugins import Plugin
 
-
 class RestartPlugin(plugins.Plugin):
     __author__ = 'Discord: terminatoror'
-    __version__ = '1.0.0'
+    __version__ = '1.1.0'
     __license__ = 'GPL3'
     __description__ = 'This plugin restarts the pwnagotchi in case bettercap crashes. Heavily based on the restart scripts made by Discord: _dal_ '
 
@@ -16,10 +16,21 @@ class RestartPlugin(plugins.Plugin):
 
     def on_loaded(self):
         logging.info('RestartPlugin loaded...')
+        #Ticker added to restart the entire device if needed
         self.running = True
+        self.error_counts = {
+            "concurrent_map": 0,
+            "runtime": 0
+        }
+        self.threshold = 3
+        self.ticker = 0
+        self.ticker_limit = 3
 
-    @staticmethod
-    def search_string_in_log(search_string, log_file):
+        while self.running:
+            self.search_log_for_errors()
+            time.sleep(30)  # Search for errors every 30 seconds
+
+    def search_string_in_log(self, search_string, log_file):
         try:
             with open(log_file, "r") as f:
                 for line in f:
@@ -29,36 +40,38 @@ class RestartPlugin(plugins.Plugin):
             logging.error(f"Error while searching log: {e}")
         return False
 
-    @on_event("beacon")
-    def search_log_for_errors(self, agent, **kwargs):
+    def restart_device(self):
+        logging.info("RestartPlugin: Restarting the device...")
+        display.set('status', 'Error detected! Restarting...')
+        subprocess.run("sudo reboot", shell=True)
+
+    def search_log_for_errors(self):
         # Search for the first error message
         search_string_concurrent = "fatal error: concurrent map iteration and map write"
         log_file_concurrent = "/var/log/syslog"
         if self.search_string_in_log(search_string_concurrent, log_file_concurrent):
-            logging.info("Found the concurrent map iteration error in syslog, executing command...")
-            # Insert your command here:
-            # For Pwnagotchi-specific operations, you can use `agent` like:
-            # agent.run(f"sudo systemctl restart bettercap pwngrid-peer && touch /root/.pwnagotchi-auto && systemctl restart pwnagotchi")
-            time.sleep(10)
-            logging.info('RestartPlugin: detected error (fatal error: concurrent map iteration and map write) restarting now...')
+            logging.info("Found the concurrent map iteration error in syslog...")
             display.set('status', 'Error detected! Restarting...')
             agent.run("sudo systemctl restart bettercap pwngrid-peer && touch /root/.pwnagotchi-auto && systemctl restart pwnagotchi")
-            # just to add some time puffer in case the error gets found
-            time.sleep(45)
-
-
+            self.error_counts["concurrent_map"] += 1
+            self.ticker += 1
 
         # Search for the second error message
         search_string_runtime = "panic: runtime error"
         log_file_runtime = "/var/log/syslog"
         if self.search_string_in_log(search_string_runtime, log_file_runtime):
-            logging.info("Found the runtime error in syslog, executing command...")
-            # Insert your command here:
-            # For Pwnagotchi-specific operations, you can use `agent` like:
-            # agent.run(f"sudo systemctl restart bettercap pwngrid-peer && touch /root/.pwnagotchi-auto && systemctl restart pwnagotchi")
-            time.sleep(10)
-            logging.info('RestartPlugin: detected error (panic: runtime error) restarting now...')
+            logging.info("Found the runtime error in syslog...")
             display.set('status', 'Error detected! Restarting...')
             agent.run("sudo systemctl restart bettercap pwngrid-peer && touch /root/.pwnagotchi-auto && systemctl restart pwnagotchi")
-            # just to add some time puffer in case the error gets found
-            time.sleep(45)
+            self.error_counts["runtime"] += 1
+            self.ticker += 1
+
+        # Restart the device if the set ticker limit is reached. Also reset the variables to avoid weird bugs.
+        if self.ticker >= self.ticker_limit:
+            self.ticker = 0
+            self.error_counts = {
+                "concurrent_map": 0,
+                "runtime": 0
+            }
+            logging.info("Error counts before restarting: concurrent map iteration and map write:" +  self.error_counts["concurrent_map"] + "panic: runtime error:" + self.error_counts["runtime"])
+            self.restart_device()
